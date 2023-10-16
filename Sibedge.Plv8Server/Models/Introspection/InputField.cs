500bc27b27ea6b03357ddebfe23dde35fa9f0f7f
Alexey Fadeev <fadeevas@sibedge.com>
2023-10-16 13:03:29 +0700

    GraphQl introspection: ported to PLV8

--- Sibedge.Plv8Server/Models/Introspection/InputField.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sibedge.Plv8Server.Models.Introspection
-{
-    /// <summary> GraphQL introspection input field </summary>
-    public class InputField : FieldBase
-    {
-        /// <summary> Default value </summary>
-        public object DefaultValue { get; set; }
-    }
-}
