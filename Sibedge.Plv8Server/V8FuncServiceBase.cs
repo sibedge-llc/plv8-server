e24a29871b899518a460b17024cae64b8a5c4fbb
Alexey Fadeev <fadeevas@sibedge.com>
2024-07-17 11:50:58 +0700

    V8FuncServiceBase.ExecuteFunction: overload with CancellationToken added

--- Sibedge.Plv8Server/V8FuncServiceBase.cs
+++ Sibedge.Plv8Server/V8FuncServiceBase.cs
@@ -2,6 +2,7 @@
 {
     using System.Data;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using Models;
     using Sibedge.Plv8Server.Helpers;
@@ -18,7 +19,18 @@
         }
 
         /// <summary> Executes SQL function </summary>
+        /// <param name="funcName"> DB function name </param>
+        /// <param name="args"> Function arguments </param>
         public Task<string> ExecuteFunction(string funcName, params object[] args)
+        {
+            return this.ExecuteFunction(funcName, CancellationToken.None, args);
+        }
+
+        /// <summary> Executes SQL function </summary>
+        /// <param name="funcName"> DB function name </param>
+        /// <param name="cancellationToken"> Propagates notification that operations should be canceled </param>
+        /// <param name="args"> Function arguments </param>
+        public Task<string> ExecuteFunction(string funcName, CancellationToken cancellationToken, params object[] args)
         {
             var argNames = string.Join(
                 ',',
@@ -34,7 +46,7 @@
                     x => $"@arg{x.i}",
                     x => x.val is FunctionArgument funcArgValue ? funcArgValue.Value : x.val.AsSqlParameter());
 
-            return this.connection.ReadJson(sql, argsDict);
+            return this.connection.ReadJson(sql, argsDict, cancellationToken);
         }
     }
 }
